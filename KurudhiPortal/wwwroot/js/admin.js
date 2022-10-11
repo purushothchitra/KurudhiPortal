0df9b6d614862f2f92c47ae938d09de075e85e4b
Purushothaman <purushothchithra1103@gmail.com>
2022-10-11 22:09:48 +0530

    creating admin management page

--- KurudhiPortal/wwwroot/js/admin.js
+++ KurudhiPortal/wwwroot/js/admin.js
@@ -1,26 +1,148 @@
 ﻿$(document).ready(function () {
-    $(".admin-nav ul li").click(function () {
-        $(".admin-nav ul li").removeClass("underline-style");
-        $(this).addClass("underline-style");
+    $("#user-filter-form").validate({
+        rules: {
+            country: {
+                required: true,
+            },
+            state: {
+                required: true,
+            },
+            district: {
+                required: true,
+            },
+            location: {
+                required: true,
+            },
+            bloodgroup: {
+                required:true,
+            }
+        },
+        messages: {
+            country: {
+                required: "Please select country",
+            },
+            state: {
+                required: "Please select state",
+            },
+            district: {
+                required: "Please select district"
+            },
+            location: {
+                required: "Please select location"
+            },
+            bloodgroup: {
+                required: "Please select bloodgroup",
+            }
+        },
+        errorElement: "div",
+        errorPlacement: function (error, element) {
+            error.addClass("invalid-feedback");
+            error.insertAfter(element);
+        },
+        highlight: function (element) {
+            $(element).addClass('border-line');
+        },
+        unhighlight: function (element) {
+            $(element).removeClass('border-line');
+        }
     });
-    $(".admin-nav ul li").click(function () {
-        $(".admin-nav ul li").removeClass("underline-style");
-        $(this).addClass("underline-style");
-    });
-    $(".admin-nav ul li").click(function () {
-        $(".admin-nav ul li").removeClass("underline-style");
-        $(this).addClass("underline-style");
-    });
-    $(".admin-nav ul li").click(function () {
-        $(".admin-nav ul li").removeClass("underline-style");
-        $(this).addClass("underline-style");
-    });
-    $(".admin-nav ul li").click(function () {
-        $(".admin-nav ul li").removeClass("underline-style");
-        $(this).addClass("underline-style");
-    });
-    $(".admin-nav ul li").click(function () {
-        $(".admin-nav ul li").removeClass("underline-style");
-        $(this).addClass("underline-style");
+    $("#signup-form").validate({
+        rules: {
+            username: {
+                required: true,
+                minlength: 8,
+                maxlength: 25,
+            },
+            password: {
+                required: true,
+                minlength: 8
+            },
+            firstname: {
+                required: true
+            },
+            lastname: {
+                required: true,
+            },
+            gender: {
+                required: true,
+            },
+            bloodgroup: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            age: {
+                required: true
+            },
+            mobilenumber: {
+                required: true,
+                minlength: 10
+            },
+            district: {
+                required: true,
+            },
+            state: {
+                required: true,
+            },
+            country: {
+                required: true
+            }
+        },
+        messages: {
+            username: {
+                required: "Please enter user name",
+                minlength: "Please enter valid user name",
+                maxlength: "Please enter correct name",
+            },
+            firstname: {
+                required: "Please enter first name"
+            },
+            lastname: {
+                required: "Please enter last name"
+            },
+            email: {
+                required: "Please enter email",
+                email: "Please enter correct email"
+            },
+            password: {
+                required: "Please enter password",
+                minlength: "Please enter valid password"
+            },
+            age: {
+                required: "Please enter age"
+            },
+            bloodgroup: {
+                required: "Please enter blood group"
+            },
+            mobilenumber: {
+                required: "Please enter the mobile number",
+                minlength: "Please enter valid mobile number"
+            },
+            district: {
+                required: "Please select district"
+            },
+            gender: {
+                required: "Please select gender"
+            },
+            country: {
+                required: "Please select country"
+            },
+            state: {
+                required: "Please select state"
+            },
+        },
+        errorElement: "div",
+        errorPlacement: function (error, element) {
+            error.addClass("invalid-feedback");
+            error.insertAfter(element);
+        },
+        highlight: function (element) {
+            $(element).addClass('border-line');
+        },
+        unhighlight: function (element) {
+            $(element).removeClass('border-line');
+        }
     });
 });
